705936d5e61f59e448a5f6734a0ca1ad5434fc59
Mark Burnett <mark.m.burnett@gmail.com>
2010-02-09 01:16:56 +0000

    Unittests for all existing end conditions added.
    git-svn-id: http://fitpy.googlecode.com/svn/trunk@5 f7dd3cde-146a-11df-9bdd-f1a4add27d5b

--- end_conditions.py
+++ end_conditions.py
@@ -1,6 +1,6 @@
 import datetime
 
-__all__ = ['Counter', 'Timer', 'StoppedIncreasing']
+__all__ = ['Counter', 'Timer', 'StoppedImproving']
 
 class Counter(object):
     """
@@ -9,9 +9,11 @@ class Counter(object):
     def __init__(self, num_generations):
         self.num_left     = num_generations
         self.num_starting = num_generations
+
     def __call__(self, best_fitness):
         self.num_left -= 1
         return 0 >= self.num_left
+
     def reset(self):
         self.num_left = self.num_starting
 
@@ -26,29 +28,39 @@ class Timer(object):
         """
         self.run_duration = run_duration
         self.finish_time  = datetime.datetime.now() + run_duration
+
     def __call__(self, best_fitness):
         return self.finish_time < datetime.datetime.now()
+
     def reset(self):
         self.finish_time  = datetime.datetime.now() + self.run_duration
 
-class StoppedIncreasing(object):
+class StoppedImproving(object):
     """
-        End condition to specify a maximum numbe rof generations to go without
-    improving the most fit individual.
+        Specify a maximum number of generations to go without improving
+    the most fit individual.
     """
     def __init__(self, static_generations):
         self.static_generations = static_generations
         self.unchanged = 0
         self.last_fitness = None
+        self.finished = False
+
     def __call__(self, best_fitness):
-        if best_fitness == self.last_fitness:
-            self.unchanged += 1
-        else:
-            self.unchanged = 0
-            self.last_fitness = best_fitness
+        if not self.finished:
+            if best_fitness == self.last_fitness:
+                self.unchanged += 1
+            else:
+                self.unchanged = 0
+                self.last_fitness = best_fitness
+
         if self.unchanged >= self.static_generations:
+            self.finished = True
             return True
+
         return False
+
     def reset(self):
         self.last_fitness = None
         self.unchanged = 0
+        self.finished = False
--- tests/end_condition_tests.py
+++ tests/end_condition_tests.py
@@ -1,5 +1,8 @@
-from fitpy import end_conditions
+import datetime
+import time
 import unittest
+
+from fitpy import end_conditions
 
 class TestCounter(unittest.TestCase):
     def testNormal(self):
@@ -17,6 +20,67 @@ class TestCounter(unittest.TestCase):
         self.assertFalse(any(c(None) for i in xrange(49)))
         self.assertTrue(c(None))
         self.assertTrue(c(None))
+
+class TestTimer(unittest.TestCase):
+    def testNormal(self):
+        delta = datetime.timedelta(milliseconds=5)
+
+        # Offset the count by one, or miss due to loop run time.
+        count = 1
+        t = end_conditions.Timer(delta)
+        while not t(None):
+            time.sleep(1e-3)
+            if not t(None):
+                count += 1
+        self.assertEqual(5, count)
+
+        count = 1
+        t.reset()
+        while not t(None):
+            time.sleep(1e-3)
+            if not t(None):
+                count += 1
+        self.assertEqual(5, count)
+
+class TestStoppedImproving(unittest.TestCase):
+    def testIncreasing(self):
+        increasing = end_conditions.StoppedImproving(3)
+        values = [1, 2, 3, 3, 3, 4, 3, 5, 5, 5, 5, 6, 6, 7, 8, 9]
+
+        for i, v in enumerate(values):
+            result = increasing(v)
+            if 10 <= i:
+                self.assertTrue(result)
+            else:
+                self.assertFalse(result)
+
+        increasing.reset()
+        for i, v in enumerate(values):
+            result = increasing(v)
+            if 10 <= i:
+                self.assertTrue(result)
+            else:
+                self.assertFalse(result)
+
+    def testDecreasing(self):
+        decreasing = end_conditions.StoppedImproving(3)
+        values = [9, 9, 9, 8, 7, 7, 6, 6, 6, 6, 5, 4, 3, 2, 2]
+
+        for i, v in enumerate(values):
+            result = decreasing(v)
+            if 9 <= i:
+                self.assertTrue(result)
+            else:
+                self.assertFalse(result)
+
+        decreasing.reset()
+        for i, v in enumerate(values):
+            result = decreasing(v)
+            if 9 <= i:
+                self.assertTrue(result)
+            else:
+                self.assertFalse(result)
+
 
 if '__main__' == __name__:
     unittest.main()

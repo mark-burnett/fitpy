fc53f44f95e37926a82f65e068e81c0135d9e112
David Morton <davidlmorton@gmail.com>
2010-07-03 21:38:33 +0000

    Bench table program edited a bit.
    git-svn-id: http://fitpy.googlecode.com/svn/trunk@44 f7dd3cde-146a-11df-9bdd-f1a4add27d5b

--- bench.py
+++ bench.py
@@ -2,6 +2,8 @@
 
 import itertools
 
+import numpy
+
 import baker
 
 from fitpy.utils import iterables
@@ -14,54 +16,64 @@ def decode_names(names, module):
     iterable_names = iterables.make_iterable(names)
     return [getattr(module, name) for name in iterable_names]
 
-def run_benchmarks(algorithm_names, benchmarks):
-    results = []
-    for algorithm_name, benchmark in itertools.product(algorithm_names,
-                                                       benchmarks):
+def run_benchmark(algorithm_name, benchmark, num_runs):
+    evaluation_counts = []
+    costs = []
+    for i in xrange(num_runs):
         full_results = optimize(benchmark.cost_function, 
                                 algorithm_name=algorithm_name, 
                                 **benchmark.kwargs)
-        useful_results = {'num_evaluations': len(full_results['evaluation_cache']),
-                          'cost': full_results['best_cost']}
-        results.append(useful_results)
+        evaluation_counts.append(len(full_results['evaluation_cache']))
+        costs.append(full_results['best_cost'])
+
+    return {'num_evaluations_average': numpy.average(evaluation_counts),
+            'num_evaluations_std': numpy.std(evaluation_counts),
+            'cost_average': numpy.average(costs),
+            'cost_std': numpy.std(costs)}
+
+def run_benchmarks(algorithm_names, benchmarks, num_runs):
+    results = []
+    for algorithm_name, benchmark in itertools.product(algorithm_names,
+                                                       benchmarks):
+        results.append(run_benchmark(algorithm_name, benchmark, num_runs))
 
     return results
 
 def display_benchmark_results(results):
     for result in results:
-        print result['num_evaluations']
-        print result['cost']
+        print result['num_evaluations_average'], result['num_evaluations_std']
+        print result['cost_average'], result['cost_std']
 #        print len(result['evaluation_cache'])
 #        print result['best_parameters']
 #        print result['best_cost']
 
-@baker.command
-def algorithm(algorithm_name, benchmark_names=None, num_runs=1):
-    """
-    :param algorithm_name: Name of the algorithm to benchmark.
-    :param benchmarks: Which benchmarks to run (default = all).
-    :param num_runs: How many times to perform each benchmark.
-    """
-    if benchmark_names is None:
-        # Get list of all benchmarks
-        benchmarks = bm.all_benchmarks
-    else:
-        benchmarks = decode_names(benchmark_names, bm)
-
-    results = run_benchmarks([algorithm_name], benchmarks)
-
-    display_benchmark_results(results)
-
-@baker.command
-def benchmark(benchmark_name, algorithms=None, num_runs=1):
-    print 'benchmark', benchmark_name
+#@baker.command
+#def algorithm(algorithm_name, benchmark_names=None, num_runs=1):
+#    """
+#    :param algorithm_name: Name of the algorithm to benchmark.
+#    :param benchmarks: Which benchmarks to run (default = all).
+#    :param num_runs: How many times to perform each benchmark.
+#    """
+#    if benchmark_names is None:
+#        # Get list of all benchmarks
+#        benchmarks = bm.all_benchmarks
+#    else:
+#        benchmarks = decode_names(benchmark_names, bm)
+#
+#    results = run_benchmarks([algorithm_name], benchmarks)
+#
+#    display_benchmark_results(results)
+#
+#@baker.command
+#def benchmark(benchmark_name, algorithms=None, num_runs=1):
+#    print 'benchmark', benchmark_name
 
 @baker.command(default=True)
 def all(num_runs=50):
     benchmarks = bm.all_benchmarks
     algorithm_names = fitpy.algorithms.algorithm_names.algorithm_names
 
-    results = run_benchmarks(algorithm_names, benchmarks)
+    results = run_benchmarks(algorithm_names, benchmarks, num_runs)
 
     display_benchmark_results(results)
 
